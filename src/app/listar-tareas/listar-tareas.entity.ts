2392673021ecf0a060e65c4e8390899574f79cb0
Nicolas Mesa Urquijo <nmesa@ibiscom.net>
2026-06-18 14:38:09 -0500

    Listar tareas Entity y service

---
+++ src/app/listar-tareas/listar-tareas.entity.ts
@@ -0,0 +1,47 @@
+/**
+ * Entidad que representa una definición de un listartarea en el sistema.
+ */
+export interface ListarTareasEntity {
+  /** id del workflow */
+  idInstanciaWorkflow: string;
+  /** Nombre del workflow */
+  nombreWorkflow: string;
+  /** Número */
+  numero: string;
+  /** Nombre  */
+  nombre: string;
+  /** Fecha de creación del listartarea */
+  estado: Date;
+  /** Id instancia workflow padre del listartarea */
+  idInstanciaWorkflowPadre: string;
+  /** Nombre workflowpadre */
+  nombreWorkflowPadre: string;
+  /** fecha asignación */
+  fechaAsignacion: Date;
+  /** nombre largo tarea */
+  nombreLargoTarea: string;
+  /** nombre largo proceso */
+  nombreLargoProceso: string;
+  /** nombre largo proceso padre */
+  nombreLargoProcesoPadre: string;
+  /** responsable*/
+  responsable: string;
+  /** valor negocio */
+  valorNegocio: string;
+  /** valor negocio 2 */
+  valorNegocio2: string;
+  /** valor negocio 3 */
+  valorNegocio3: string;
+  /** valor negocio 4 */
+  valorNegocio4: string;
+  /** días vencimiento */
+  diasVencimiento: string;
+  /** imagenes semaforo */
+  imagenesSemaforo: string[];
+    /** fecha desde */
+  fechaDesde: string[];
+    /** fecha hasta */
+  fechaHasta: string[];
+
+}
+
---
+++ src/app/listar-tareas/listar-tareas.service.ts
@@ -0,0 +1,131 @@
+import { Injectable } from '@angular/core';
+import { GroupSearchFilterEntity } from '../entities/groups/group-search-filter.entity';
+import { FsResponseEntity } from '../entities/backend/fs-response.entity';
+import { GroupEntity } from '../entities/groups/group.entity';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../environments/environment';
+import { ListarTareaEntity, ListarTareasEntity } from './listar-tareas.entity';
+import { ListarTareaFilterEntity } from './listar-tareas-filter.entity';
+
+@Injectable({
+  providedIn: 'root',
+})
+/**
+ * Servicio para la administración de listartarea.
+ * Incluye búsquedas y CRUD.
+ */
+export class ListarTareaService {
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+  ) {}
+
+  /**
+   * Crea un nuevo listartarea.
+   * @param listartarea La información del listartarea a crear.
+   * @returns 
+   */
+  public CargarListarTarea(listartarea: ListarTareaEntity): Observable<FsResponseEntity<any>> {
+      let ip: string = this.cookieService.get('ip');
+      return this.http.put<FsResponseEntity<any>>(
+        environment.listartareaApiUrl +
+          `/taskList/loadTask`, listartarea
+      );
+  }
+
+  /**
+   * Obtiene el listado de listartarea según los filtros proporcionados.
+   * @param filtros Los filtros para la búsqueda de listartarea.
+   * @returns 
+   */
+  public getListarTarea(filtros: ListarTareaFilterEntity): Observable<FsResponseEntity<any>> {
+      let ip: string = this.cookieService.get('ip');
+      return this.http.post<FsResponseEntity<any>>(
+        environment.listartareaApiUrl +
+          `/taskList/getAssignedTasks`, filtros
+      );
+  }
+
+  /**
+   * Obtiene los atributos del proceso.
+   * @param atributoListarTareas El nombre del workflow al que pertenece el objeto workflow a obtener.
+   * @returns Observable con la respuesta del servidor y el objeto workflow.
+   */
+  public obtenerAtributo(atributoListarTareas: string): Observable<FsResponseEntity<ListarTareasEntity>> {
+    let ip: string = this.cookieService.get('ip');
+    return this.http.get<FsResponseEntity<ListarTareasEntity>>(
+      environment.workflowApiUrl + 
+        `/taskList/getInfoWorkflow${atributoListarTareas}`,
+    );
+  }
+
+    /**
+   * Obtiene los atributos del proceso.
+   * @param informacionTarea El nombre del workflow al que pertenece el objeto workflow a obtener.
+   * @returns Observable con la respuesta del servidor y el objeto workflow.
+   */
+  public obtenerInformacion(informacionListarTareas: string): Observable<FsResponseEntity<ListarTareasEntity>> {
+    let ip: string = this.cookieService.get('ip');
+    return this.http.get<FsResponseEntity<ListarTareasEntity>>(
+      environment.workflowApiUrl + 
+        `/taskList/getInfoTask{informacionListarTareas}`,
+    );
+  }
+
+
+     /**
+   * Obtiene los documentos del proceso.
+   * @param atributosTarea El nombre del workflow al que pertenece el objeto workflow a obtener.
+   * @returns Observable con la respuesta del servidor y el objeto workflow.
+   */
+  public obtenerAtributos(atributosListarTareas: string): Observable<FsResponseEntity<ListarTareasEntity>> {
+    let ip: string = this.cookieService.get('ip');
+    return this.http.get<FsResponseEntity<ListarTareasEntity>>(
+      environment.workflowApiUrl + 
+        `/taskList/getDocsWorkflow{atributosListarTareas}`,
+    );
+  }
+
+       /**
+   * Obtiene los documentos del proceso.
+   * @param documentosTarea El nombre del workflow al que pertenece el objeto workflow a obtener.
+   * @returns Observable con la respuesta del servidor y el objeto workflow.
+   */
+  public obtenerDocumentos(documentosListarTareas: string): Observable<FsResponseEntity<ListarTareasEntity>> {
+    let ip: string = this.cookieService.get('ip');
+    return this.http.get<FsResponseEntity<ListarTareasEntity>>(
+      environment.workflowApiUrl + 
+        `/taskList/getDocsTask{documentosListarTareas}`,
+    );
+  }
+
+         /**
+   * Obtiene los documentos del evento de inicio.
+   * @param documentoseventoTarea El nombre del workflow al que pertenece el objeto workflow a obtener.
+   * @returns Observable con la respuesta del servidor y el objeto workflow.
+   */
+  public obtenerDocumentosEvento(documentoseventoTareas: string): Observable<FsResponseEntity<ListarTareasEntity>> {
+    let ip: string = this.cookieService.get('ip');
+    return this.http.get<FsResponseEntity<ListarTareasEntity>>(
+      environment.workflowApiUrl + 
+        `/taskList/getDocsStart{documentoseventoTarea}`,
+    );
+  }
+  
+
+
+  /**
+   * Elimina un listartarea, dado su identificador
+   * @param listartareaName Nombre del listartarea a eliminar
+   * @returns 
+   */
+  public deleteListarTarea(listartareaName: string): Observable<FsResponseEntity<any>> {
+      let ip: string = this.cookieService.get('ip');
+      return this.http.delete<FsResponseEntity<any>>(
+        environment.listartareaApiUrl +
+          `/listartarea/delete?listartareaName=${listartareaName}`);
+  }
+}
+
